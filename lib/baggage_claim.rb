1801ac3deb875d08bcb78242eff0e6ef32c40bc9
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2022-04-21 09:19:39 -0400

    first commit

---
+++ lib/baggage_claim.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "baggage_claim/version"
+
+module BaggageClaim
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/baggage_claim/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BaggageClaim
+  VERSION = "0.1.0"
+end
